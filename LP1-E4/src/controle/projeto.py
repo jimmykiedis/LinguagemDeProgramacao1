b4b1704907b987aa2d15d7e4cdafe5bd000729fc
jimmykiedis <123708476+jimmykiedis@users.noreply.github.com.>
2026-06-08 22:15:21 -0400

    Removidas funções de salvamento e carregamento de arquivos; adicionado arquivo binário para orçamento de peças.

--- LP1-E4/src/controle/projeto.py
+++ LP1-E4/src/controle/projeto.py
@@ -1,22 +1,8 @@
-import pickle
 from util.persistência_arquivo import carregar_arquivo, salvar_arquivo
 from entidades.seguradora import get_seguradoras, set_seguradoras
 from entidades.sinistro import get_sinistros, set_sinistros
 from entidades.orcamento import get_orçamentos, set_orcamentos
 from interfaces.interface_textual import loop_opções_execução
-
-def salvar_arquivo(nome_arquivo, objetos):
-    arquivo = open('../../dados/' + nome_arquivo + 'bin', 'wb')
-    pickle.dump(objetos, arquivo)
-    arquivo.close()
-
-def carregar_arquivo(nome_arquivo):
-    try:
-        arquivo = open('../../dados/' + nome_arquivo + '.bin', 'rb')
-        objetos = pickle.load(arquivo)
-    except IOError:
-        objetos = None
-    return objetos
 
 nome_arquivo = 'orçamento_peças'
 
